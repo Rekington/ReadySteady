693f8645c3679e72e153fc27f86ff75e7c39268d
Rekington <50083829+Rekington@users.noreply.github.com>
2021-10-23 14:06:33 +0100

    This is the baseline and wireframe level of starting point that we were looking for.

---
+++ app/src/main/java/com/example/readysteady/PassengerActivity.java
@@ -0,0 +1,4 @@
+package com.example.readysteady;
+
+public class PassengerActivity {
+}
---
+++ app/src/main/java/com/example/readysteady/WaitingActivity.java
@@ -0,0 +1,25 @@
+package com.example.readysteady;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WaitingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WaitingActivity.this,PassengerActivity.class));
+                finish();
+            }
+        }, 4000);
+    }
+}
